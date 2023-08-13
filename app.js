b12dbb5718cd1534ca5aca3a9b87d9d9f7cb15a0
skazgor <ruhulazgor@gmail.com>
2023-08-13 15:36:36 +0600

    event

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express")
 const app = express()
 const env=require('dotenv').config();
 const plan = require("./routes/plan_route")
+const event = require("./routes/event_route")
 
 
 app.use(express.json())
 app.use("/plan", plan)
+app.use("/event", event)
 
 const port = process.env.PORT || 3000
 app.listen(port, console.log(`Server started on port ${port}`))
