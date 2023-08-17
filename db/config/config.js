8d73a3ad8b14db8eda7b69cd166edd09987e0547
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2023-08-18 01:55:22 +0600

    trying the same still

--- db/config/config.js
+++ db/config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
       }
     },
   },
