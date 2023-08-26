7149b404f7755f9a6b5ae33de889939544bbf7f3
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2023-08-27 01:57:57 +0600

    new api : explore other regions

--- controllers/plan.controller.js
+++ controllers/plan.controller.js
@@ -82,113 +82,9 @@ module.exports = {
             return;
           }
 
-          // get all the places in the regions
-          const places = await models.Place.findAll({
-            where: {
-              region_id: regions.map(region => region.region_id),
-              type: 'spot'
-            },
-            attributes: ['id', 'title', 'rating', 'rating_count']
-          });
+          const regionIds = regions.map(region => region.region_id);
 
-          if (!places) {
-            res.status(404).send('Places in the regions not found');
-            return;
-          }
-
-          // get all the activities in those places
-          const activities = await models.PlaceActivity.findAll({
-            where: {
-              place_id: places.map(place => place.id)
-            }
-          });
-
-          if (!activities) {
-            res.status(404).send('Activities for the places in the regions in the plan not found');
-            return;
-          }
-
-          // get all the tags
-          const tags = await models.Tag.findAll({
-            where: {
-              id: activities.map(activity => activity.tag_id)
-            }
-          });
-
-          if (!tags) {
-            res.status(404).send('Tags not found');
-            return;
-          }
-
-          // get all the images of the places
-          const images = await models.PlaceImage.findAll({
-            where: {
-              place_id: places.map(place => place.id)
-            }
-          });
-          // images can be empty
-
-          // get all the events of the plan to check whether the activity is already in the plan
-          const events = await models.Event.findAll({
-            where: {
-              plan_id: plan_id
-            }
-          });
-
-          if (!events) {
-            res.status(404).send('Events in the plan not found');
-            return;
-          }
-
-          const tagsPlacesActivities = [];
-
-          for (let i=0; i<tags.length; i++) {
-            const tag = tags[i];
-            const tag_id = tag.id;
-
-            const tagPlaces = [];
-            for (let j=0; j<places.length; j++) {
-              const place = places[j];
-              const place_id = place.id;
-
-              const placeActivities = [];
-              for (let k=0; k<activities.length; k++) {
-                const activity = activities[k];
-
-                // check whether the activity is already in the plan
-                let in_plan = events.find(event => event.place_id === place_id 
-                  && event.activity_id === activity.activity_id) === undefined;
-
-                // only add the activity if it is not in the plan already
-                if (!in_plan && activity.tag_id === tag_id && activity.place_id === place_id) {
-                  placeActivities.push({
-                    id: activity.activity_id,
-                    title: activity.activity_title,
-                  });
-                }
-              }
-
-              if (placeActivities.length > 0) {
-                tagPlaces.push({
-                  id: place_id,
-                  title: place.title,
-                  rating: place.rating,
-                  rating_count: place.rating_count,
-                  images: images.filter(image => image.place_id === place_id).map(image => image.link),
-                  activities: placeActivities
-                });
-              }
-            }
-
-            if (tagPlaces.length > 0) {
-              tagsPlacesActivities.push({
-                tag_id: tag_id,
-                tag_name: tag.title,
-                places: tagPlaces
-              });
-            }
-          }
-
+          const tagsPlacesActivities = await calculateExploration(res, plan_id, regionIds);
           res.status(200).send(tagsPlacesActivities);
 
         } catch (e) {
@@ -203,7 +99,189 @@ module.exports = {
 
     },
 
-  exploreOtherRegion: async (req, res) => {
+  getExploreOtherRegions: async (req, res) => {
     // take those regions that are not in the plan's region but in the plan's region's nearby region
+    try {
+      const plan_id = req.query.plan_id;
+
+      try {
+        const planRegions = await models.PlanRegion.findAll({
+          where: {
+            plan_id: plan_id
+          }
+        });
+
+        if (!planRegions) {
+          res.status(404).send('Plan not found');
+          return;
+        }
+
+        const nearbyRegions = await models.NearbyRegion.findAll({
+          where: {
+            [models.Sequelize.Op.or]: [
+              { first_region_id: planRegions.map(region => region.region_id) },
+              { second_region_id: planRegions.map(region => region.region_id) }
+            ]
+          }
+        });
+
+        if (!nearbyRegions) {
+          res.status(200).send([]);
+        }
+
+        // get unique nearby region ids excluding the plan's region ids
+        const nearbyRegionIds = new Set();
+        nearbyRegions.forEach(nearbyRegion => {
+          nearbyRegionIds.add(nearbyRegion.first_region_id);
+          nearbyRegionIds.add(nearbyRegion.second_region_id);
+        });
+        nearbyRegionIds.forEach(nearbyRegionId => {
+          planRegions.forEach(planRegion => {
+            if (planRegion.region_id === nearbyRegionId) {
+              nearbyRegionIds.delete(nearbyRegionId);
+            }
+          });
+        });
+
+        // get all the regions
+        const regions = await models.Region.findAll({
+          where: {
+            id: [...nearbyRegionIds]
+          }
+        });
+
+        result = [];
+        for (let nearbyRegionId of nearbyRegionIds) {
+          const regionResult = await calculateExploration(res, plan_id, nearbyRegionId);
+          result.push({
+            region_id: nearbyRegionId,
+            region_name: regions.find(region => region.id === nearbyRegionId).title,
+            tags_places_activities: regionResult
+          });
+        }
+
+        res.status(200).send(result);
+
+      } catch (e) {
+        console.log('Explore other region error: ', e);
+        res.status(500).send('Internal server error');
+      }
+
+    } catch (e) {
+      console.log('Explore other region error: ', e);
+      res.status(400).send('Bad request');
+    }
   }
+}
+
+async function calculateExploration(res, plan_id, regionIds) {
+
+  // get all the places in the regions
+  const places = await models.Place.findAll({
+    where: {
+      region_id: regionIds,
+      type: 'spot'
+    },
+    attributes: ['id', 'title', 'rating', 'rating_count']
+  });
+
+  if (!places) {
+    res.status(404).send('Places in the regions not found');
+    return;
+  }
+
+  // get all the activities in those places
+  const activities = await models.PlaceActivity.findAll({
+    where: {
+      place_id: places.map(place => place.id)
+    }
+  });
+
+  if (!activities) {
+    res.status(404).send('Activities for the places in the regions in the plan not found');
+    return;
+  }
+
+  // get all the tags
+  const tags = await models.Tag.findAll({
+    where: {
+      id: activities.map(activity => activity.tag_id)
+    }
+  });
+
+  if (!tags) {
+    res.status(404).send('Tags not found');
+    return;
+  }
+
+  // get all the images of the places
+  const images = await models.PlaceImage.findAll({
+    where: {
+      place_id: places.map(place => place.id)
+    }
+  });
+  // images can be empty
+
+  // get all the events of the plan to check whether the activity is already in the plan
+  const events = await models.Event.findAll({
+    where: {
+      plan_id: plan_id
+    }
+  });
+
+  if (!events) {
+    res.status(404).send('Events in the plan not found');
+    return;
+  }
+
+  const tagsPlacesActivities = [];
+
+  for (let i = 0; i < tags.length; i++) {
+    const tag = tags[i];
+    const tag_id = tag.id;
+
+    const tagPlaces = [];
+    for (let j = 0; j < places.length; j++) {
+      const place = places[j];
+      const place_id = place.id;
+
+      const placeActivities = [];
+      for (let k = 0; k < activities.length; k++) {
+        const activity = activities[k];
+
+        // check whether the activity is already in the plan
+        let in_plan = events.find(event => event.place_id === place_id
+          && event.activity_id === activity.activity_id) != undefined;
+
+        // only add the activity if it is not in the plan already
+        if (!in_plan && activity.tag_id === tag_id && activity.place_id === place_id) {
+          placeActivities.push({
+            id: activity.activity_id,
+            title: activity.activity_title,
+          });
+        }
+      }
+
+      if (placeActivities.length > 0) {
+        tagPlaces.push({
+          id: place_id,
+          title: place.title,
+          rating: place.rating,
+          rating_count: place.rating_count,
+          images: images.filter(image => image.place_id === place_id).map(image => image.link),
+          activities: placeActivities
+        });
+      }
+    }
+
+    if (tagPlaces.length > 0) {
+      tagsPlacesActivities.push({
+        tag_id: tag_id,
+        tag_name: tag.title,
+        places: tagPlaces
+      });
+    }
+  }
+
+  return tagsPlacesActivities;
 }
