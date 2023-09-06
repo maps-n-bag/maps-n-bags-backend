f9a1a1668168c784f97e94cb623d08d6ad60be14
skazgor <ruhulazgor@gmail.com>
2023-09-06 10:03:21 +0600

    plan creation done

--- controllers/create.plan.js
+++ controllers/create.plan.js
@@ -129,15 +129,13 @@ module.exports.createPlan = async(body)=>{
             region_id:regions[i]
         });
     }
-    let currentTimestamp = new Date(body.start_date);
-    currentTimestamp.setHours(9,0,0,0);
     for(let i=1;i<=number_of_days;i++){
         if(plan_serialized.length==0){
             break;
         }
         let activity_count = MAX_ACTIVITY_PER_DAY;
-        let plan_activity_for_current_day = [{}];
-        while(activity_count>0){
+        let plan_activity_for_current_day = [];
+        while(activity_count>0 && plan_serialized.length!=0){
             let place=plan_serialized[0];
             let activity=placeActivities.filter((placeActivity)=>{
                 return placeActivity.place_id==place;
@@ -149,16 +147,90 @@ module.exports.createPlan = async(body)=>{
             else if(activity.length<=activity_count){
                 activity_count = activity_count-activity.length;
                 plan_serialized.shift();
-                plan_activity_for_current_day.push(activity);
+                plan_activity_for_current_day.push(...activity);
             }
             else{
+                for(let m=0;m<activity_count;m++){
+                    plan_activity_for_current_day.push(activity[m]);
+                    placeActivities.splice(placeActivities.indexOf(activity[m]),1);
+                }
                 activity_count = 0;
-                plan_activity_for_current_day.push(activity.slice(0,activity_count));
-                placeActivities = placeActivities.splice(placeActivities.indexOf(activity),activity_count);
             }
         }
         console.log(plan_activity_for_current_day);
+        let total_journey_time = 0;
+        for(let j=0;j<plan_activity_for_current_day.length-1;j++){
+            if(plan_activity_for_current_day[j].place_id==plan_activity_for_current_day[j+1].place_id){
+                continue;
+            }
+            let distance = distances.filter((distance)=>{
+                return (distance.first_place_id==plan_activity_for_current_day[j].place_id && distance.second_place_id==plan_activity_for_current_day[j+1].place_id) ||
+                (distance.first_place_id==plan_activity_for_current_day[j+1].place_id && distance.second_place_id==plan_activity_for_current_day[j].place_id);
+            });
+            total_journey_time+=distance[0].est_time;
+        }
+        let start_time =0;
+        if(total_journey_time>2.5*60){
+            start_time=total_journey_time-2.5*60;
+        }
+        let end_time = 0;
+        if(start_time>1.5*60){
+            end_time = start_time-1.5*60;
+        }
+        let total_activity_time = start_time+end_time+8.5*60-total_journey_time;
+        let activity_time = total_activity_time/plan_activity_for_current_day.length;
+        let currentTimestamp = new Date(body.start_date);
+        currentTimestamp.setDate(currentTimestamp.getDate()+i-1);
+        currentTimestamp.setHours(9,30,0,0);
+        console.log(currentTimestamp);
+        console.log(start_time);
+        console.log(activity_time);
+        console.log(total_journey_time);
+        currentTimestamp = subtractTime(currentTimestamp,Math.floor(start_time/60),start_time%60,0,0);
+        for(let j=0;j<plan_activity_for_current_day.length;j++){
+            await models.Event.create({
+                plan_id:plan.id,
+                activity_id:plan_activity_for_current_day[j].activity_id,
+                place_id:plan_activity_for_current_day[j].place_id,
+                start_time:currentTimestamp,
+                day:i,
+                end_time: addTime(new Date(currentTimestamp),Math.floor(activity_time/60),activity_time%60,0,0)
+            });
+            currentTimestamp = addTime(currentTimestamp,Math.floor(activity_time/60),activity_time%60,0,0);
+            if(j!=plan_activity_for_current_day.length-1 && plan_activity_for_current_day[j].place_id!=plan_activity_for_current_day[j+1].place_id){
+                let distance = distances.filter((distance)=>{
+                    return (distance.first_place_id==plan_activity_for_current_day[j].place_id && distance.second_place_id==plan_activity_for_current_day[j+1].place_id) ||
+                    (distance.first_place_id==plan_activity_for_current_day[j+1].place_id && distance.second_place_id==plan_activity_for_current_day[j].place_id);
+                });
+                currentTimestamp = addTime(currentTimestamp,Math.floor(distance[0].est_time/60),distance[0].est_time%60,0,0);
+            }
+            console.log(currentTimestamp);
+        }
     }
+}
+const subtractTime = (timestamp, hours, minutes, seconds, milliseconds) => {
+    timestamp.setHours(timestamp.getHours() - hours);
+    const totalMinutes = timestamp.getMinutes() - minutes;
+    if(totalMinutes<0){
+        timestamp.setHours(timestamp.getHours()-1);
+        timestamp.setMinutes(60+totalMinutes);
+    }
+    else{
+        timestamp.setMinutes(totalMinutes);
+    }
+    return timestamp;
+}
+const addTime = (timestamp, hours, minutes, seconds, milliseconds) => {
+    timestamp.setHours(timestamp.getHours() + hours);
+    const totalMinutes = timestamp.getMinutes() + minutes;
+    if(totalMinutes>60){
+        timestamp.setHours(timestamp.getHours()+1);
+        timestamp.setMinutes(totalMinutes-60);
+    }
+    else{
+        timestamp.setMinutes(totalMinutes);
+    }
+    return timestamp;
 }
 
 const getNearbyPlaces = async(fromPLaceId, allPlaces, distances)=>{
@@ -298,11 +370,14 @@ const findMatchingPlaceActivitiesUserPreference = async (props) => {
 }
 
 const findMatchingPlaceActivitiesDefault = async (props) => {
-    const tags = await models.Tag.findAll({
+    let tags = await models.Tag.findAll({
         order: [
             ['id', 'ASC'],
         ],
     });
+    tags=tags.map((tag)=>{
+        return tag.dataValues.id
+    })
     let totalPlaces = props.number_of_event;
     let totalPlacesActivityArray = [];
     let interestedPlaces = await models.Place.findAll({
@@ -317,12 +392,15 @@ const findMatchingPlaceActivitiesDefault = async (props) => {
     interestedPlaces = interestedPlaces.map((place) => {
         return place.dataValues.id;
     });
-    for (let i = 0; i < tags.length && totalPlaces != 0; i++) {
+    for (let i = 0; i < tags.length && totalPlaces != 0; i+=2) {
         const placeActivities = await models.PlaceActivity.findAll({
             where: {
-                tag_id: tags[i].id
+                tag_id: tags[i]
             },
-            attributes: ['place_id', 'activity_id']
+            attributes: ['place_id', 'activity_id'],
+            order:[
+                ['tag_id','ASC']
+            ]
         });
         if(placeActivities.length>0){
             if(placeActivities.length<totalPlaces){
@@ -346,9 +424,27 @@ const findMatchingPlaceActivitiesDefault = async (props) => {
             }
             else{
                 for(let j=0;j<interestedPlaces.length && totalPlaces!=0;j++){
-                    if(interestedPlaces.includes(placeActivities[j].dataValues.place_id)){
-                        totalPlacesActivityArray.push(placeActivities[j].dataValues)
-                        totalPlaces = totalPlaces - 1;
+                    let targetpair=[]
+                    for(let m=0;m<placeActivities.length;m++){
+                        if(interestedPlaces[j]==placeActivities[m].dataValues.place_id){
+                            targetpair.push(placeActivities[m].dataValues)
+                        }
+                    }
+                    if(targetpair.length>0){
+                        let temp_bool = false;
+                        for(let l=0;l<targetpair.length&& totalPlaces!=0;l++){
+                            for (let k=0;k<totalPlacesActivityArray.length;k++){
+                                if(totalPlacesActivityArray[k].place_id==targetpair[l].place_id && totalPlacesActivityArray[k].activity_id== targetpair[l].activity_id){
+                                    temp_bool = true;
+                                    continue;
+                                }
+                            }
+                            if(temp_bool){
+                                continue;
+                            }
+                            totalPlacesActivityArray.push(targetpair[l])
+                            totalPlaces = totalPlaces -1;
+                        }
                     }
                 }
             }
