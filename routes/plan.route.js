2137618b501b5d2e63c8874915cd9badcf51a287
Ishika Tarin Ime <64859699+IshikaIme@users.noreply.github.com>
2023-09-11 15:02:05 +0600

    auth

--- routes/plan.route.js
+++ routes/plan.route.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const auth = require('../middleware/auth.middleware')
-const controller = require('../controllers/plan.controller')
+const auth = require("../middleware/auth.middleware");
+const controller = require("../controllers/plan.controller");
 
-router.post('/', auth, controller.createPlan)
-router.get('/', auth, controller.getPlan)
-router.get('/all', auth, controller.getAllPlans)
-router.delete('/', auth, controller.deletePlan)
-router.get('/explore/other', auth, controller.getExploreOtherRegions)
-router.get('/explore', auth, controller.getExplorations)
-router.post('/update', auth, controller.updatePlan)
-router.put('/edit', auth, controller.editPlan)
-router.put('/edit/public', auth, controller.togglePlanPublic)
-router.post('/others', auth, controller.getOtherPlans)
-router.post('/copy', auth, controller.copyPlan)
-router.get('/generateBlog', auth, controller.getGenerateBlog)
+router.post("/", auth, controller.createPlan);
+router.get("/", auth, controller.getPlan);
+router.get("/all", auth, controller.getAllPlans);
+router.delete("/", auth, controller.deletePlan);
+router.get("/explore/other", auth, controller.getExploreOtherRegions);
+router.get("/explore", auth, controller.getExplorations);
+router.post("/update", auth, controller.updatePlan);
+router.put("/edit", auth, controller.editPlan);
+router.put("/edit/public", controller.togglePlanPublic);
+router.post("/others", auth, controller.getOtherPlans);
+router.post("/copy", auth, controller.copyPlan);
+router.get("/generateBlog", auth, controller.getGenerateBlog);
 
-module.exports = router
+module.exports = router;
