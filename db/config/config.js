1a84de9cefba699c8617f3a3ff91e0f2060044a8
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2025-03-05 01:23:48 +0600

    Update config.js
    as the db is no longer in render but on neon, changed the db host to external from internal

--- db/config/config.js
+++ db/config/config.js
@@ -28,7 +28,7 @@ module.exports = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST_INTERNAL,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
